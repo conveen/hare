328324f08ec2a64b55aef5ba04fc92cd57be5b75
conveen <conveen@protonmail.com>
2020-11-14 00:53:09 -0600

    Updated Dockerfile and entrypoint to substitute env variables in OpenSearch template

--- hare-engine/deploy/entrypoint.sh
+++ hare-engine/deploy/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+# If HARE_DOMAIN provided, fill in template and remove it.
+if ! [ -z "${HARE_DOMAIN}" ]
+then
+    if [ -z "${DEFAULT_SEARCH_ENGINE}" ]; then DEFAULT_SEARCH_ENGINE="google"; fi
+    envsubst < hare_engine/static/opensearch.xml.template > hare_engine/static/opensearch.xml
+fi
+rm hare_engine/static/opensearch.xml.template
+
 envsubst < /etc/nginx/sites-available/hare_engine.conf.template > /etc/nginx/sites-available/hare_engine.conf && \
     rm -f /etc/nginx/sites-available/hare_engine.conf.template && \
     ln -s /etc/nginx/sites-available/hare_engine.conf /etc/nginx/sites-enabled/hare_engine.conf && \
