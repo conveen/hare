343c198bc183f3da079643c5c299e62463dc72a8
conveen <conveen@protonmail.com>
2020-11-17 17:41:23 -0600

    Changed entrypoint and Nginx/uWSGI config to output uWSGI logs to container stdout with Apache-style records.

--- hare-engine/deploy/entrypoint.sh
+++ hare-engine/deploy/entrypoint.sh
@@ -11,5 +11,5 @@ rm hare_engine/static/opensearch.xml.template
 envsubst < /etc/nginx/sites-available/hare_engine.conf.template > /etc/nginx/sites-available/hare_engine.conf && \
     rm -f /etc/nginx/sites-available/hare_engine.conf.template && \
     ln -s /etc/nginx/sites-available/hare_engine.conf /etc/nginx/sites-enabled/hare_engine.conf && \
-    uwsgi --ini /var/www/hare/hare_engine.ini && \
-    nginx -g 'daemon off;'
+    nginx && \
+    uwsgi --ini /var/www/hare/hare_engine.ini
