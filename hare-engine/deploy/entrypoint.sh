8736cd7e20c6e55504926030a97e71a85ebfdcba
conveen <conveen@protonmail.com>
2020-11-18 00:36:26 -0600

    Add option to install DB driver in venv before connection.

--- hare-engine/deploy/entrypoint.sh
+++ hare-engine/deploy/entrypoint.sh
@@ -8,6 +8,12 @@ then
 fi
 rm hare_engine/static/opensearch.xml.template
 
+# Install database driver if specified
+if ! [ -z "${HARE_DATABASE_DRIVER}" ]
+then
+    . venv/bin/activate && pip install --no-cache-dir "${HARE_DATABASE_DRIVER}" && deactivate
+fi
+
 envsubst < /etc/nginx/sites-available/hare_engine.conf.template > /etc/nginx/sites-available/hare_engine.conf && \
     rm -f /etc/nginx/sites-available/hare_engine.conf.template && \
     ln -s /etc/nginx/sites-available/hare_engine.conf /etc/nginx/sites-enabled/hare_engine.conf && \
