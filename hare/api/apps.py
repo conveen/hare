5076559fab6e78d1eaf6f9276078d2ad12512050
conveen <conveen@protonmail.com>
2021-06-19 23:38:01 -0500

    Move hare to core, engine_api to api, and engine_ui to ui

--- hare/api/apps.py
+++ hare/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EngineUiConfig(AppConfig):
+class EngineApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'engine_ui'
+    name = 'api'
