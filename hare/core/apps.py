678d6a7590d5aaf3a41c05c560c710c0b12d2d65
conveen <conveen@protonmail.com>
2021-08-15 10:52:15 -0500

    Implement /health route in core module (#23)

--- hare/core/apps.py
+++ hare/core/apps.py
@@ -20,27 +20,9 @@
 ## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ## SOFTWARE.
 
-"""hare URL Configuration
+from django.apps import AppConfig
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
 
-from hare.core import views
-
-urlpatterns = [
-    path("health/", views.health_check, name="health-check"),
-    path("admin/", admin.site.urls),
-]
+class CoreConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hare.core"
