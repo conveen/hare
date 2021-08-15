80edb10385aa4b760353deb54efc8a24a5fc86e1
conveen <conveen@protonmail.com>
2021-08-15 11:40:32 -0500

    Move settings and configuration to hare.conf module (#24)

--- hare/conf/__init__.py
+++ hare/conf/__init__.py
@@ -19,20 +19,3 @@
 ## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 ## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ## SOFTWARE.
-
-"""
-WSGI config for hare project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hare.core.settings")
-
-application = get_wsgi_application()
