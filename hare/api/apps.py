dede1f327031904b599cd407067c31d9948dbf3f
conveen <conveen@protonmail.com>
2021-08-15 10:46:49 -0500

    Rename UI and API AppConfig classes

--- hare/api/apps.py
+++ hare/api/apps.py
@@ -23,6 +23,6 @@
 from django.apps import AppConfig
 
 
-class EngineApiConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "hare.api"
--- hare/ui/apps.py
+++ hare/ui/apps.py
@@ -23,6 +23,6 @@
 from django.apps import AppConfig
 
 
-class EngineUiConfig(AppConfig):
+class UiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "hare.ui"
