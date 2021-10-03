0722bed9fb75f58692e698135982608934670bd6
conveen <conveen@protonmail.com>
2021-10-03 10:58:30 -0500

    Change run_code_check to run commands in paralle (#36)

--- hare/core/management/commands/run_code_check.py
+++ hare/core/management/commands/run_code_check.py
@@ -21,20 +21,58 @@
 ## SOFTWARE.
 
 import logging
-import subprocess
+import multiprocessing as mp
+from pathlib import Path
+import typing
 
 from django.core.management import base as command
+from django.core.management.color import no_style
+
+from hare.core.management import subprocess_command
+from hare.core.management.commands import (
+    run_black,
+    run_mypy,
+    run_pylint,
+)
 
 
 logger = logging.getLogger(__name__)
 
+NO_STYLE = no_style()
+TOOLS = {
+    "black": run_black.Command,
+    "mypy": run_mypy.Command,
+    "pylint": run_pylint.Command,
+}
 
-class Command(command.BaseCommand):
+
+class DelayedExecutor:
+    """Curry Django command execution by first providing state (args, kwargs)."""
+
+    __slots__ = (
+        "args",
+        "command_cls",
+        "kwargs",
+    )
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, command_cls: typing.Type[command.BaseCommand]) -> None:
+        command_cls().execute(*self.args, **self.kwargs)
+
+
+class Command(subprocess_command.SubprocessCommand):
     help = "Run CI tools on source files - Black, Pylint, and MyPy."
 
     def handle(self, *args, **options) -> None:
-        for tool_name in ("black", "pylint", "mypy"):
-            try:
-                subprocess.run(["python", "manage.py", f"run_{tool_name}"], check=True)
-            except subprocess.CalledProcessError as exc:
-                logger.warning("Failed to run tool {} (exited with code {})", tool_name, exc.returncode)
+        source_paths_gen: typing.Generator[Path, None, None] = options["source_paths"]
+        source_paths: typing.List[str] = [f"{source_path}" for source_path in source_paths_gen]
+
+        options.pop("stdout", None)
+        options.pop("stderr", None)
+        options["source_paths"] = source_paths
+
+        with mp.Pool(processes=len(TOOLS)) as pool:
+            pool.map(DelayedExecutor(**options), TOOLS.values())
