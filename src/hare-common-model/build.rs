c38469dff9da4215dbe01bfef589bb8a826f5899
conveen <conveen@protonmail.com>
2024-10-11 00:40:20 -0500

    Bump tonic to latest major version

--- src/hare-common-model/build.rs
+++ src/hare-common-model/build.rs
@@ -3,6 +3,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(false)
         .build_server(false)
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(&["../../proto/hare/pagination.proto"], &["../../proto/hare"])?;
+        .compile_protos(&["../../proto/hare/pagination.proto"], &["../../proto/hare"])?;
     Ok(())
 }
