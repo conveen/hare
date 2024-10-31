1504173a562c4c71be42a2109fd9c8055e510ba9
conveen <conveen@protonmail.com>
2024-10-31 17:38:37 -0500

    Add serde Serialize derive to all common and control plane types

--- src/hare-common-model/build.rs
+++ src/hare-common-model/build.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_client(false)
         .build_server(false)
+        .type_attribute(".", "#[derive(serde::Serialize)]")
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&["../../proto/hare/pagination.proto"], &["../../proto/hare"])?;
     Ok(())
--- src/hare-control-plane-model/build.rs
+++ src/hare-control-plane-model/build.rs
@@ -1,5 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
+        .type_attribute(".", "#[derive(serde::Serialize)]")
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&["../../proto/hare/control_plane.proto"], &["../../proto/hare"])?;
     Ok(())
