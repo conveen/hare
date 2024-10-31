5633a08a17e534c0b28db4e8cc023a07a98e66f8
conveen <conveen@protonmail.com>
2024-10-31 17:44:21 -0500

    Update control-data plane glue code to properly convert data plane Shortcut list

--- src/hare-control-plane-service/src/convert.rs
+++ src/hare-control-plane-service/src/convert.rs
@@ -27,17 +27,25 @@ impl FromDataPlane<CommittedShortcut> for hare_control_plane_model::Shortcut {
 
 impl FromDataPlane<CommittedShortcutList> for Vec<hare_control_plane_model::Shortcut> {
     fn convert(committed_shortcut_list: CommittedShortcutList) -> Self {
-        committed_shortcut_list.shortcuts.into_iter().map(|committed_shortcut| hare_control_plane_model::Shortcut {
-            uid: committed_shortcut.destination.uid,
-            url: committed_shortcut.destination.url,
-            num_params: committed_shortcut.destination.num_params as i32,
-            is_fallback: committed_shortcut.destination.is_fallback,
-            is_default_fallback: committed_shortcut.destination.is_default_fallback,
-            description: committed_shortcut.destination.description,
-            aliases: committed_shortcut.aliases.into_iter().map(|committed_alias| hare_control_plane_model::Alias {
-                uid: committed_alias.uid,
-                name: committed_alias.name,
-            }).collect(),
-        }).collect()
+        committed_shortcut_list
+            .shortcuts
+            .into_iter()
+            .map(|committed_shortcut| hare_control_plane_model::Shortcut {
+                uid: committed_shortcut.destination.uid,
+                url: committed_shortcut.destination.url,
+                num_params: committed_shortcut.destination.num_params as i32,
+                is_fallback: committed_shortcut.destination.is_fallback,
+                is_default_fallback: committed_shortcut.destination.is_default_fallback,
+                description: committed_shortcut.destination.description,
+                aliases: committed_shortcut
+                    .aliases
+                    .into_iter()
+                    .map(|committed_alias| hare_control_plane_model::Alias {
+                        uid: committed_alias.uid,
+                        name: committed_alias.name,
+                    })
+                    .collect(),
+            })
+            .collect()
     }
 }
