cda002ed7330867e019f9127850d75ec7bd31063
conveen <conveen@protonmail.com>
2025-10-12 10:58:10 -0500

    Bump to tonic 0.14 and enable TLS in the CLI

--- src/hare-control-plane-model/build.rs
+++ src/hare-control-plane-model/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize)]")
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&["../../proto/hare/control_plane.proto"], &["../../proto/hare"])?;
